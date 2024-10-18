226bdfd4c4868572656d733c8755281901da3176
minhwankwak <rhkralsghks777@gmail.com>
2024-10-19 05:09:04 +0900

    [버그/개선] 버그 및 개선사항 수정

--- Assets/BanpoFri/Scripts/Table/Define/EventInfo.cs
+++ Assets/BanpoFri/Scripts/Table/Define/EventInfo.cs
@@ -7,53 +7,60 @@ namespace BanpoFri
     public class EventInfoData
     {
         [SerializeField]
-		private int _eventid;
-		public int eventid
+		private int _event_id;
+		public int event_id
 		{
-			get { return _eventid;}
-			set { _eventid = value;}
+			get { return _event_id;}
+			set { _event_id = value;}
 		}
 		[SerializeField]
-		private int _stageId;
-		public int stageId
+		private int _stage_id;
+		public int stage_id
 		{
-			get { return _stageId;}
-			set { _stageId = value;}
+			get { return _stage_id;}
+			set { _stage_id = value;}
 		}
 		[SerializeField]
-		private string _eventType;
-		public string eventType
+		private string _event_type;
+		public string event_type
 		{
-			get { return _eventType;}
-			set { _eventType = value;}
+			get { return _event_type;}
+			set { _event_type = value;}
 		}
 		[SerializeField]
-		private string _eventSubtype;
-		public string eventSubtype
+		private string _event_subtype;
+		public string event_subtype
 		{
-			get { return _eventSubtype;}
-			set { _eventSubtype = value;}
+			get { return _event_subtype;}
+			set { _event_subtype = value;}
 		}
 		[SerializeField]
-		private int _eventTypeValue;
-		public int eventTypeValue
+		private int _event_weight;
+		public int event_weight
 		{
-			get { return _eventTypeValue;}
-			set { _eventTypeValue = value;}
+			get { return _event_weight;}
+			set { _event_weight = value;}
 		}
 		[SerializeField]
-		private int _eventDurationMs;
-		public int eventDurationMs
+		private int _event_type_value;
+		public int event_type_value
 		{
-			get { return _eventDurationMs;}
-			set { _eventDurationMs = value;}
+			get { return _event_type_value;}
+			set { _event_type_value = value;}
 		}
 		[SerializeField]
-		private string _randomEventText;
-		public string randomEventText
+		private int _event_duration;
+		public int event_duration
 		{
-			get { return _randomEventText;}
-			set { _randomEventText = value;}
+			get { return _event_duration;}
+			set { _event_duration = value;}
+		}
+		[SerializeField]
+		private string _event_description;
+		public string event_description
+		{
+			get { return _event_description;}
+			set { _event_description = value;}
 		}
 
     }
