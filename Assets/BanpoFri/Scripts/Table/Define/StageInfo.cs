edc4320feaa2c3f3de80b68ce4d44ae45ec49c5b
minhwankwak <rhkralsghks777@gmail.com>
2024-10-19 02:26:32 +0900

    [버그/수정]

--- Assets/BanpoFri/Scripts/Table/Define/StageInfo.cs
+++ Assets/BanpoFri/Scripts/Table/Define/StageInfo.cs
@@ -14,22 +14,22 @@ namespace BanpoFri
 			set { _stage_id = value;}
 		}
 		[SerializeField]
-		private string _start_price;
-		public string start_price
+		private int _start_price;
+		public int start_price
 		{
 			get { return _start_price;}
 			set { _start_price = value;}
 		}
 		[SerializeField]
-		private string _start_money;
-		public string start_money
+		private int _start_money;
+		public int start_money
 		{
 			get { return _start_money;}
 			set { _start_money = value;}
 		}
 		[SerializeField]
-		private string _target_money;
-		public string target_money
+		private int _target_money;
+		public int target_money
 		{
 			get { return _target_money;}
 			set { _target_money = value;}
@@ -63,32 +63,32 @@ namespace BanpoFri
 			set { _change_up_rate = value;}
 		}
 		[SerializeField]
-		private int _down_speed_min;
-		public int down_speed_min
+		private int _down_stock_min;
+		public int down_stock_min
 		{
-			get { return _down_speed_min;}
-			set { _down_speed_min = value;}
+			get { return _down_stock_min;}
+			set { _down_stock_min = value;}
 		}
 		[SerializeField]
-		private int _down_speed_max;
-		public int down_speed_max
+		private int _down_stock_max;
+		public int down_stock_max
 		{
-			get { return _down_speed_max;}
-			set { _down_speed_max = value;}
+			get { return _down_stock_max;}
+			set { _down_stock_max = value;}
 		}
 		[SerializeField]
-		private int _up_speed_min;
-		public int up_speed_min
+		private int _up_stock_min;
+		public int up_stock_min
 		{
-			get { return _up_speed_min;}
-			set { _up_speed_min = value;}
+			get { return _up_stock_min;}
+			set { _up_stock_min = value;}
 		}
 		[SerializeField]
-		private int _up_speed_max;
-		public int up_speed_max
+		private int _up_stock_max;
+		public int up_stock_max
 		{
-			get { return _up_speed_max;}
-			set { _up_speed_max = value;}
+			get { return _up_stock_max;}
+			set { _up_stock_max = value;}
 		}
 		[SerializeField]
 		private List<int> _event_time;
