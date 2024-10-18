4086fbebb60a6b2f315a13477594e2d99a047c8b
HighCl <fdsa1469@naver.com>
2024-10-19 04:05:53 +0900

    기능 추가: 이벤트 실행 시 값 변경 기능 추가

--- Assets/Script/Game/Data/StockEventData/StockEventData.cs
+++ Assets/Script/Game/Data/StockEventData/StockEventData.cs
@@ -26,78 +26,22 @@ public class StockEventData
     public void EndEvent()
     {
         SetData(false);
-
         _callback.Invoke(_eventInfoData);
     }
 
     //TODO: Start인 경우 의도한 값 그대로 세팅하고, 끝나는 경우 -1 곱하기
     private void SetData(bool isStart)
     {
-        switch (_eventInfoData.eventid)
+        int sign = isStart ? 1 : -1;
+
+        switch (_eventInfoData.eventType)
         {
-            case 1:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
+            case "changeStageValue":
+                if (_eventInfoData.eventSubtype == "increaseSpeedMin")
+                    StockEventSystem.SpeedMin += _eventInfoData.eventTypeValue * sign;
+                if (_eventInfoData.eventSubtype == "changeDownRate")
+                    StockEventSystem.DownRate += _eventInfoData.eventTypeValue * sign;
                 break;
-            case 2:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-            case 3:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-            case 4:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-            case 5:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-            case 6:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-
-            case 7:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 8:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 9:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-            case 10:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 11:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 12:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 13:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 14:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 15:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 16:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
-            case 17:
-                Debug.Log($"HighCl_{Time.time}: SetData\nEvent{_eventInfoData.eventid} Data Setting");
-                break;
-
             default:
                 break;
         }
